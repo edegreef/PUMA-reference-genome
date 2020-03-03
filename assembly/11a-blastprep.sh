b911cd22b1dac36335d96c514aff0d00ed036210
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2020-03-03 10:18:18 -0600

    Update 11a-blastprep.sh

--- assembly/11a-blastprep.sh
+++ assembly/11a-blastprep.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+genome=/scratch/user/edegreef/ref_genome/PUMA2_03022020.fasta
+
 #downloading taxdb file from NCBI, which will be used in the $BLASTDB path in blast step
 wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/taxdb.tar.gz
 tar -xzf taxdb.tar.gz
@@ -10,7 +14,7 @@ mv taxdb.tar.gz ncbi/taxdb.tar.gz
 
 #splitting genome per scaffold
 mkdir split_dir
-csplit -n 1 -f split_dir/ /scratch/user/edegreef/ref_genome/PUMA2_03022020.fasta "%^>%" "/^>/" "{*}" -s 
+csplit -n 1 -f split_dir/ $genome "%^>%" "/^>/" "{*}" -s 
 ls -thor split_dir/ | awk '{print $8}' | sed '1d' > list_contigs
 
 #for the 2,896 scaffolds, I manually split the list_contigs into 5 lists (A-E) to run multiple jobs (11b-blastn.lsf) at once to save time
