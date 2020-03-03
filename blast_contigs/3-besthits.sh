fdac453ffa8584ffc3cb80a957cfc5192e8cd81f
Evelien de Greef <49288304+edegreef@users.noreply.github.com>
2020-03-02 19:51:00 -0600

    Delete 3-besthits.sh

--- blast_contigs/3-besthits.sh
+++
@@ -1,5 +0,0 @@
-#combining all the best_hit outputs into one
-cat *_bestHits > allbesthits.txt
-
-#searching for anything that is not Eukaryota for contamination
-grep -v Eukaryota allbesthits.txt > contaminants.txt
